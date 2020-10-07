e4318b98bb0c8e7b37e9ff7055413074273c7577
Roger <roger.alex.aguilar@gmail.com>
2020-10-07 13:44:16 -0700

    ready for deployment

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const connectToMongoDB = require("./config/db");
+const path = require("path");
 
 connectToMongoDB();
 
@@ -8,11 +9,18 @@ const app = express();
 // to be able to use req.body middleware
 app.use(express.json({ extended: false }));
 
-app.get("/", (req, res) => res.send("API is Online"));
 app.use("/registration", require("./routes/registration"));
 app.use("/login", require("./routes/login"));
 app.use("/entry", require("./routes/entry"));
 app.use("/profile", require("./routes/deleteProfile"));
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server Running on Port ${PORT}`));
