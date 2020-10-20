f8c4e70db5cec7a56d4e923885a0c66bb8538bde
Roger <roger.alex.aguilar@gmail.com>
2020-10-20 14:59:10 -0700

    added serapartion in footnote

--- client/src/components/Footnote.js
+++ client/src/components/Footnote.js
@@ -9,11 +9,11 @@ const Footnote = () => {
           <Col className="text-center py-4 border-top">
             <h4>© 2020 - A douschesois creation</h4>
             <div className="footnote-links">
-              <a href="https://www.douschesois.com/">Website</a>
+              <a href="https://www.douschesois.com/">Website</a>|
               <a href="https://www.linkedin.com/in/roger-alexander-37925619a/">
                 Linkedin
               </a>
-              <a href="https://github.com/DouscheSois">Github</a>
+              |<a href="https://github.com/DouscheSois">Github</a>
             </div>
           </Col>
         </Row>
