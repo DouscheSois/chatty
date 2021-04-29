15a7cd2d809be935093633d01688f238bf48cf28
Roger <roger.alex.aguilar@gmail.com>
2021-04-28 19:56:16 -0700

    deploy ready 1.3

--- client/src/screens/LoginScreen.js
+++ client/src/screens/LoginScreen.js
@@ -33,7 +33,7 @@ const Login = ({ location, history }) => {
 
   return (
     <FormContainer>
-      <h1>Sign In</h1>
+      <h1>Login</h1>
       {error && <Message variant="danger">{error}</Message>}
       {loading && <Loader />}
       <Form onSubmit={submitHandler}>
